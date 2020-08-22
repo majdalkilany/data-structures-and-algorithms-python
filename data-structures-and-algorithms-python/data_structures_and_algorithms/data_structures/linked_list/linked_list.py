aaa90dd077bc60fa564f8a72cc7b34b5b1158663
majdalkilany <majdkilany92@gmail.com>
2020-08-22 23:53:36 +0300

    done

--- data-structures-and-algorithms-python/data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data-structures-and-algorithms-python/data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,64 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-    # put your LinkedList implementation here
-    pass
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def __str__(self):
+        current = self.head
+        output = ''
+        while current:
+            output += f"{ {str(current.value)} } ->"
+            current = current.next
+        return output
+
+
+    def insert(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+
+            new_node = Node(value)
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+
+    def includes(self, values):
+        current_node = self.head
+        while current_node.next :
+            if current_node.value == values:
+                return True
+            else:
+                current_node = current_node.next
+        return False
+
+
+if __name__ == "__main__":
+    ll =LinkedList()
+    ll.append(4)
+    print(ll)
+    ll.append('mm')
+    ll.insert('m')
+    ll.insert('f')
+    ll.append('d')
+    mm = LinkedList()
+    ll.append(4)
+    print(ll)
+    print(ll.includes(4))
