e8fe4ec92432d19cd83282181b40d773eb555912
majdalkilany <majdkilany92@gmail.com>
2020-09-05 15:11:32 +0300

    fio_animal

--- data-structures-and-algorithms-python/data_structures_and_algorithms/challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ data-structures-and-algorithms-python/data_structures_and_algorithms/challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -9,8 +9,6 @@ class Dog :
         self.name = name 
 
     def __str__(self):
-        return f'{self.name}'
-    def str_dog(self) :
         return 'dog'
 
 
@@ -20,92 +18,104 @@ class Cat :
         self.name = name 
 
     def __str__(self):
-        return f'{self.name}'
-    def str_cat(self) :
         return 'cat'
 
 
+class Queue:
+    def __init__(self):
+        self._front = None
+        self._rear = None
 
-class Stack :
-    def __init__(self) :
+    def enqueue(self, value):
+        """add new value to front of linked list"""
+        node = Node(value)
 
-        self.top = None
+        if self._rear: 
+            self._rear.next = node
+            self._rear = node
+
+        else:
+            self._rear = self._front = node
+
+    def dequeue(self):
+        """removes node from linked list"""
+        if not self._front:
+            raise Exception('cannot dequeue an empty stack')
+
+        exiting = self._front
+        self._front = self._front.next
+        return exiting.value
+
+    def peek(self):
+        """returns front Node vlaue, first in line"""
+        if not self._front:
+            raise Exception('cannot peek an empty stack')
+
+        return self._front.value
+
+    def is_empty(self):
+        """returns bool if queue is empty"""
+        return not self._front 
+
+
     def __str__(self):
         res = ""
-        current = self.top
+        current = self._front
         while current:
             res += f"{ [current.value]} -> "
             current = current.next
         return res 
-
-    def push (self,value) : 
-        '''
-        it takes one value as argument and push it to the top of the stack
-        '''
-
-        if self.top == None : 
-            self.top = Node(value)
-        else :
-            new_node = Node(value)
-            new_node.next = self.top
-            self.top = new_node
-
-
-
-
-    def is_empty(self) :
-        '''
-            returns boolen if the stack is empty or not
-        '''
-
-        return self.top is None 
-
-
-
-
-    def pop(self):
-        """removes node on top of stack and returns that value"""
-        if not self.top:
-            raise Exception('cannot pop an empty stack')
-
-        if self.top:
-            outgoing = self.top
-            self.top = self.top.next
-            return outgoing.value
-
-
-
-    def peek(self):
-        """returns value at top of stack"""
-        return self.top.value
 
 
 
 class AnimalShelter : 
 
     def __init__(self):
-        self.stck = Stack()
-        self.temp = []
+        self.queue_dogs = Queue()
+        self.queue_cats = Queue()
+        self.not_animal = []
+
 
 
     def enqueue(self, animal):
-        self.stck.push(animal)
-        print(self.stck)
+        if  animal.__str__() == 'dog' :
+             self.queue_dogs.enqueue(animal) 
+        elif  animal.__str__() == 'cat' :
+             self.queue_cats.enqueue(animal) 
+        else :
+             return 'the input must be cat or dog'
 
+        self.not_animal.append(animal)
 
     def dequeue(self, pref):
-        d= Dog.str_dog('d')
-        c=Cat.str_cat('c')
-        if pref == d or pref == c :
-            while self.stck.peek() != pref :
-                self.temp.append(self.stck.pop())
-            an = self.stck.pop()
-            while self.temp != [] :
-                self.stck.push(self.temp[-1])
-            print(self.stck)
-            return an 
-        else : 
-            return None
+        dog_1= Dog('dog_1')
+        cat_1=Cat('dog_1')
+        if  pref.__str__() == 'dog' :
+              return self.queue_dogs.dequeue() 
+        elif  pref.__str__() == 'cat' :
+              return self.queue_cats.dequeue() 
+        else :
+                return None
+
+    def __str__(self):
+        res = ""
+        current = self._front
+        while current:
+            res += f"{ [current.value]} -> "
+            current = current.next
+        return res 
+
+
+        # if pref == d or pref == c :
+        #     while self.stck.peek() != pref :
+        #         self.temp.append(self.stck.pop())
+        #     an = self.stck.pop()
+        #     while self.temp != [] :
+        #         self.stck.push(self.temp[-1])
+        #     print(self.stck)
+        #     return an 
+        # else : 
+        #     return None
 
 
 
@@ -119,19 +129,19 @@ class AnimalShelter :
 
 if __name__ == "__main__":
     meme = Cat('meme')
-    print(meme.str_cat())
+    print(meme.__str__())
     print(isinstance(meme, Cat))
     oscar = Dog('oscar')
-    print(oscar.str_dog())
+    print(oscar.__str__())
     ll = AnimalShelter()
     ll.enqueue(oscar)
     ll.enqueue(meme)
-    # print(ll)
+    print(ll)
     m = Dog('m')
     v = Dog('v')
     r = Dog('r')
     ll.enqueue(m)
     ll.enqueue(v)
     ll.enqueue(r)
-    
+    print(ll)
     # ll.dequeue('cat')
